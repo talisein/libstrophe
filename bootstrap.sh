9be0b29abb74a2453c4158f4c940beb4384cec0e
Dawid Ciężarkiewicz <dpc@ucore.info>
2010-07-28 21:31:35 +0200

    Cleanup autotools scripts.

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# for git users
-[ -d .git ] && [ ! -d expat/.git ] && git submodule init . && git submodule update
-
-aclocal
-libtoolize
-automake --add-missing --foreign --copy
-autoconf
